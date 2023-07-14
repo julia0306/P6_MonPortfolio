14a115847d0012d1aa42ecb3f05709c406d653e9
Julia Taylor <julia.taylor3@gmail.com>
2023-07-14 12:49:22 +0200

    react snap config changed to take hash router into account

--- react-snap.config.js
+++ react-snap.config.js
@@ -1,11 +1,50 @@
-module.exports={snapshots: [
-    '/',
-    '/about',
-    '/projects',
-    '/project/:id',
-    '/skills', 
-    '/curriculum',
-    '/contact',
-    '/*'
-  ],
-}
+module.exports = {
+    source: './build',
+    destination: './build',
+    routes: {
+      '/': {
+        initial: true,
+        inlineCss: true,
+      },
+      '/about': {
+        inlineCss: true,
+      },
+      '/projects': {
+        inlineCss: true,
+      },
+      '/project/:id': {
+        inlineCss: true,
+      },
+      '/skills': {
+        inlineCss: true,
+      },
+      '/curriculum': {
+        inlineCss: true,
+      },
+      '/contact': {
+        inlineCss: true,
+      },
+      '/Portfolio/': {
+        initial: true,
+        inlineCss: true,
+      },
+      '/Portfolio/about': {
+        inlineCss: true,
+      },
+      '/Portfolio/projects': {
+        inlineCss: true,
+      },
+      '/Portfolio/project/:id': {
+        inlineCss: true,
+      },
+      '/Portfolio/skills': {
+        inlineCss: true,
+      },
+      '/Portfolio/curriculum': {
+        inlineCss: true,
+      },
+      '/Portfolio/contact': {
+        inlineCss: true,
+      },
+    },
+  };
